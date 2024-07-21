18b77c11b51385c7ef25c1d7422e05cdb6377c8a
siva-tanikonda <siva.jai.tanikonda@gmail.com>
2024-07-21 12:40:26 -0700

    Tweaked aiming code

--- Training/trainer.h
+++ Training/trainer.h
@@ -1,1 +1,9 @@
-#include "evaluation_manager.o"
+#include "Shared/evaluation_manager.h"
+
+class Trainer {
+    public:
+        Trainer(const json &config);
+        ~Trainer();
+    private:
+        
+};
